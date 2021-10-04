9eb38e336e3d93e8bfc01356689373324728eda3
Sowbhakian <sowbhakian.shansan@gmail.com>
2021-10-04 20:53:31 +0530

    Software Engineering Assignment completed!

---
+++ animation.js
@@ -0,0 +1,25 @@
+// Sign in - Login form
+
+
+function signup() {
+    $("#signupForm").removeClass("deactive")
+    $("#loginForm").addClass("deactive")
+
+    $("#signupBar").addClass("white")
+    $("#loginBar").removeClass("white")
+
+    $("#loginTxt").removeClass("text-decoration-underline")
+    $("#signupTxt").addClass("text-decoration-underline")
+
+}
+
+function login() {
+    $("#signupForm").addClass("deactive")
+    $("#loginForm").removeClass("deactive")
+
+    $("#signupBar").removeClass("white")
+    $("#loginBar").addClass("white")
+
+    $("#signupTxt").removeClass("text-decoration-underline")
+    $("#loginTxt").addClass("text-decoration-underline")
+}
